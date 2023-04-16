ab6c2c56446fdd7a70acd8108e0bf336c1cbc57d
Hernyik Richárd <h763803@stud.u-szeged.hu>
2023-04-16 16:02:22 +0200

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { 
+    path: 'home', 
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: 'profile', 
+    loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfileModule) 
+  },
+  {
+    path: 'not-found', 
+    loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule) 
+  },
+  {
+    path: '', 
+    redirectTo: '/home',
+    pathMatch: 'full', 
+  },
+  {
+    path: '**',
+    redirectTo: '/not-found',
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
