d31f476ad4b3c1555ca4086a5a9480610a6cc83b
Hernyik Richárd <h763803@stud.u-szeged.hu>
2023-05-07 19:32:17 +0200

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './shared/services/auth.guard';
+import { LoginGuard } from './shared/services/login.guard';
 
 const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule),
-
+    canActivate: [AuthGuard],
   },
   {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule),
-    
+    canActivate: [LoginGuard],
   },
   {
     path: 'registration',
     loadChildren: () => import('./pages/regist/regist.module').then(m => m.RegistModule),
-    
+    canActivate: [LoginGuard],
   },
   {
     path: 'not-found', 
--- src/app/shared/services/auth.guard.ts
+++ src/app/shared/services/auth.guard.ts
@@ -20,11 +20,12 @@ export class AuthGuard implements CanActivate {
       take(1),
       filter((user): user is User => user !== null && user !== undefined),
       map((user: User) => {
-        if (user.role !== 'admin') {
-          this.router.navigate(['unauthorized']);
+        if (user.role === 'admin' || user.role === 'user') {
+          return true;
+        } else {
+          this.router.navigate(['login']);
           return false;
         }
-        return true;
       })
     );
   }
