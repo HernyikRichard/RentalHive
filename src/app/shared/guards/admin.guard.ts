b4b1659bc0824a1ca70a8d61b93b749fa0a366fb
Hernyik Richárd <h763803@stud.u-szeged.hu>
2023-05-12 15:15:24 +0200

    ...

--- src/app/shared/guards/admin.guard.ts
+++ src/app/shared/guards/admin.guard.ts
@@ -1,13 +1,11 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { Observable, filter, map, take } from 'rxjs';
 import { User } from '../interfaces/User';
 import { AuthService } from '../services/auth.service';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AdminAuthGuard implements CanActivate {
+@Injectable({  providedIn: 'root' })
+export class AdminAuthGuard  {
   constructor (
       private authService: AuthService,
       private router: Router
